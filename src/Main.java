ffca19e8d138b0cacb99757c6a2bac1acf705ba0
Rafael Abednego <rafaelabednego123@gmail.com>
2025-04-22 13:39:43 +0700

    Done

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,21 @@ public class Main {
         graphMatrix.addEdge(0, 1);
         graphMatrix.addEdge(1, 2);
         graphMatrix.addEdge(2, 3);
-        graphMatrix.addEdge(3, 2);
         graphMatrix.addEdge(3, 1);
+        graphMatrix.addEdge(3, 2);
         graphMatrix.addEdge(3, 4);
         graphMatrix.addEdge(4, 0);
 
+        graphList.addEdge(1, 2);
+        graphList.addEdge(2, 3);
+        graphList.addEdge(3, 4);
+        graphList.addEdge(4, 2);
+        graphList.addEdge(4, 3);
+        graphList.addEdge(4, 5);
+        graphList.addEdge(5, 1);
+
         graphMatrix.printMatrix();
+        System.out.println();
+        graphList.printGraph();
     }
 }
